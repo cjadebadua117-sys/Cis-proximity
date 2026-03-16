2e8e2043dbc3362373b2133a6809918e8bb87267
atticus341 <eugenegurion3@gmail.com>
2026-03-16 19:55:59 +0800

    updated

--- swrs_config/settings.py
+++ swrs_config/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cisproximity',         
         'USER': 'root',                
-        'PASSWORD': 'Password',    
+        'PASSWORD': 'password',    
         'HOST': '127.0.0.1',           
         'PORT': '3306',
     }
